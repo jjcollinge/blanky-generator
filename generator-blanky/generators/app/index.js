fe293a9ee83740c4f906a6a27053cd3fa1ab0e10
Joni Collinge <jonathan-collinge@hotmail.com>
2016-02-04 08:58:41 -0800

    Moved the variable intialisation before output

--- generator-blanky/generators/app/index.js
+++ generator-blanky/generators/app/index.js
@@ -7,20 +7,19 @@ var path = require('path');
 
 module.exports = yeoman.Base.extend({
     copying: function () {
-        console.log(this.templatePath('blanky'));
-        console.log(this.destinationRoot());
-        
-        ncp.limit = 16;
-        
         var src = this.templatePath('blanky');
         var des = this.destinationRoot();
         
-            ncp(src, des, function (err) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log("Success!");
-            });
+        console.log("Copying all files from " + src + " to destination path " + des);
+
+        ncp.limit = 16;
+
+        ncp(src, des, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("Success!");
+        });
     },
 
     install: function () {
